72f62c08d4de510006808ee44c9a96c6f7abf52a
p1xelmind <trubichkinek@gmail.com>
2026-01-08 20:27:24 +1000

    commit

--- python_data_processing_toolkit/loader.py
+++ python_data_processing_toolkit/loader.py
@@ -1,6 +1,20 @@
 from pathlib import Path
-import cvs
+import csv
 import json
 
-def load_text(path: Path) -> str:
-    return path.read_text(encoding="utf-8")
+def load_text(path: Path):
+    suffix = path.suffix.lower()
+
+    if suffix == ".txt":
+        return path.read_text(encoding="utf-8")
+
+    if suffix == ".json":
+        with path.open(encoding="utf=8") as file:
+            return json.load(file)
+    
+    if suffix == ".csv":
+        with path.open(encoding="utf=8", newline="") as file:
+            reader = csv.DictReader(file)
+            return list(reader)
+
+    
