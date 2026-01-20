d5977f830b9657850f2d65ca9aecd9e628d0a136
p1xelmind <trubichkinek@gmail.com>
2026-01-20 18:02:44 +1000

    commit

--- python_data_processing_toolkit/schema.py
+++ python_data_processing_toolkit/schema.py
@@ -1,8 +1,12 @@
 from dataclasses import dataclass
 from typing import Any, Tuple, Type
 
-@dataclass
+@dataclass(frozen=True)
 class FieldSchema:
     name: str
     types: Tuple[Type, ...]
     required: bool = True
+
+@dataclass(frozen=True)
+class RecordSchema:
+    fields: Tuple[FieldSchema, ...]
